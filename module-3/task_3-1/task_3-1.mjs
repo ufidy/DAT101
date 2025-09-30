044d4fc83d8247809c6954dc7dfce1959388eb5c
ufidy <ievasi@uia.no>
2025-09-30 12:18:14 +0200

    iloveLEARNING

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -3,40 +3,85 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+let wakeUpTime = 7; 
+if (wakeUpTime === 7) {
   printOut("I can take the bus to school.");
+  } else if (wakeUpTime === 8) {
+  printOut("I can take the train to school.")
+} else {
+  printOut("I must take the car to school.");
 }
 printOut(newLine);
-
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let number = -4;
+if (number > 0) {
+  printOut("Positive");
+} else if (number < 0) {
+  printOut("Negative");
+} else {
+  printOut("Zero");
+}
+
+printOut(newLine);
+printOut("--- Part 6, 7 ----------------------------------------------------------------------------------------------");
+let r = Math.floor(Math.random()*8 + 1);
+
+if (r >= 4 && r <= 6) {
+  printOut(r + "MP" + " Thank you!");
+} else if (r > 6) {
+  printOut(r + "MP" + " The image is too big.");
+} else printOut(r + "MP" + " The image is too small.");
+
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 8, 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const monthList =["January", "February", "March", "April", "May",
+"June", "July", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+if (monthName.includes("r")) {
+  printOut(monthName + " You must take vitamin D.");
+} else {
+  printOut(monthName + " You don't need to take vitamin D.");
+}
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
 
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+switch (monthName) {
+  case "February":
+    printOut(monthName + " has 28 days, except in leap years when it has 29 days.");
+  break 
 
-/* Task 10*/
+  case "April":
+  case "June":
+  case "September":
+  case "November":
+    printOut(monthName + " has 30 days.");  
+  break
+
+    case "January":
+  case "March":
+  case "May":
+  case "July":
+  case "August":
+  case "October":
+  case "December":
+    printOut(monthName + " has 31 days.");
+  break
+  printOut(newLine);
+}
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const openMonths = ["January", "February", "September", "July", "June", "August", "October", "November", "December"];
+const closedMonths = ["March", "May"];
+
+if (openMonths.includes(monthName)) {
+  printOut( monthName + ", Gallery is open.");
+} else if (closedMonths.includes(monthName)) {
+  printOut(monthName + ", Gallery is closed.");
+} else {
+  printOut(monthName + ", Temporary premises in the building next door.");
+}
 printOut(newLine);
